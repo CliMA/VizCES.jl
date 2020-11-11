ace606d59077217e12b65719a3e5fd4a31edef7c
odunbar <odunbar@caltech.edu>
2020-11-11 05:50:57 -0800

    updated trainpts_objective_plotter.jl

--- src/trainpts_objective_plotter.jl
+++ src/trainpts_objective_plotter.jl
@@ -1,6 +1,7 @@
-include("../src/eki.jl")
-include("../src/truth.jl")
-include("../src/transforms.jl")
+include("../src/ekp.jl") # for the ekpobj
+include("../src/truth.jl") #for the covariance structure
+include("../src/transforms.jl") # for transform_prior_to_real, transform_real_to_prior
+include("../src/units.jl") # for apply_units_transform
 
 using Plots
 using Plots.PlotMeasures
@@ -13,135 +14,95 @@ using Statistics
 
 using .EKI
 
+"""
+Plotting script for the Parameter Ensemble and the GP,
+
+produces two plots,
+
+Requires:
+--------
+- EKP object
+- parameter grid defining the bounds of the plotting region
+- gp prediction of mean on the parameter grid
+- gp prediction of covariance on the parameter grid
+- transformations and inverse transformations real->prior space (if required)
+- units transform to add units (if required)
+- truthobject (the covariances)
+"""
 
 ######################################################################33
 function main()
 
-    #variables
-    homedir = split(pwd(),"/utils")[1]*"/"
-    outdir = homedir*"output/"
+    @load "ekp.jld2" ekpobj
+    @load "truth.jld2" truthobj
+    
+    @load "mean_mesh.jld2" mean_mesh
+    @load "var_mesh.jld2" var_mesh    
+    @load "param1vals.jld2" param1vals    
+    @load "param2vals.jld2" param1vals    
 
-    disc = "T21"
-    tdisc = "T21"
-    res = "l"
-    numlats = 32
-#    exp_id = "designs_"*disc*"_param_grid_1600_from_value"
-    exp_id = "designs_"*disc*"_ysample_seed300"
-    truth_id = "_phys"
-    ekidir = outdir*"eki_"*tdisc*"truth"*truth_id*"_"*res*exp_id*"/"
-    gpdir = ekidir
-    truthdir = ekidir #outdir*"eki_"*tdisc*"truth"*truth_id*"_"*res*"designs_"*disc*"_ysample_seed300/"
-
+    
     #Plotting parameters
     min_eki_it = 1
     max_eki_it = 6
-    true_params_raw = [0.7,7200]
-    true_params = [transform_rh(true_params_raw[1]),transform_t(true_params_raw[2])]
    
-    #load the ensemble parameters
-    @load ekidir*"eki.jld2" ekiobj
+    #contour plot levels
+    n_levels = 30
+   
     yt = ekiobj.g_t #the inflated sample used in eki
     # the truth
     @load truthdir*"truth.jld2" truthobj
-    gcm_truth_cov = 0.5*truthobj.cov
-    obs_truth_cov = 0.5*truthobj.cov
-
-    #the outputs from the gp
-    @load gpdir*"mean_mesh.jld2" mean_mesh
-    @load gpdir*"var_mesh.jld2" var_mesh    
-    @load gpdir*"rhumvals.jld2" rhumvals    
-    @load gpdir*"logtauvals.jld2" logtauvals  
-
-    taubound_raw = [2000,3600*3.75]
-    taubound = transform_t.(taubound_raw)
-     
-    tau_in_bd = (logtauvals .<= taubound[2]) .& (logtauvals .>= taubound[1]) 
-    logtauvals = logtauvals[tau_in_bd]
-    mean_mesh = mean_mesh[:,:,tau_in_bd]
-    var_mesh = var_mesh[:,:,tau_in_bd]
-    
+    gcm_truth_cov = cov(truthobj.sample)
+    obs_truth_cov = truthobj.cov - gcm_truth_cov
+   
     u = ekiobj.u[min_eki_it:max_eki_it]#it x [enssize x param]
     u_flat = cat(u...,dims=1)#[(itxens) x param]
     ens_size = ekiobj.J
-    # u_mesh = collect(Iterators.product(rhumvals,logtauvals)) #makes a 'meshgrid' of rhum and log vals.
-    #gp type mean, sample_mean
-    gp_type = "mean"
-    sample_size = 1000 # if sample_mean chosen
-    truth_var_transformed = zeros(length(truthobj.mean))
-    #we use long term mean as truth
-
-    #@load gpdir*"inflation.jld2" inflation_only_cov
-    #noise_sample = rand(MvNormal(zeros(length(truthobj.mean)),Diagonal(diag(inflation_only_cov))), 1) #noise_samples: size data x samples 
-     #Random.seed!(100)
-    #Random.seed!(200)       
-    #Random.seed!(100)
-    #sample_ind=randperm!(collect(1:length(truthobj.sample)))[1]
-    #yt = truthobj.sample[sample_ind] + noise_sample[:,1]
 
     # meshes, see emulate_sample.jl, "emulator_uncertainty_graphs(...)"
     # GP mean and sd in svd - transformed coordinates
     # mesh = [outputdim x N x N ]
     # We calculate the objective function by 
     # Obj(i,j) = 0.5sum_{k=1}^outputdim [ sd(i,j)^{-2} (y(k) - GP(k,i,j))^2 ] 
-    # note we do not account for the additional terms from prior+normalization constant
+    # note we do not account for the additional terms from prior
     
     # transform y into SVD coordinates first.
-    SVD = svd(gcm_truth_cov)#svd.U * svd.S * svd.Vt (can also get Vt)
+    SVD = svd(gcm_truth_cov) #svd.U * svd.S * svd.Vt (can also get Vt)
     Dinv = Diagonal(1.0 ./ sqrt.(SVD.S)) #diagonal matrix of 1/eigenvalues
     y_transform = Dinv*SVD.Vt*yt
     
-    objective = zeros((length(rhumvals),length(logtauvals)))
-    for i =1:length(rhumvals)
-        for j = 1:length(logtauvals)
-            if gp_type == "mean"
-                diff= mean_mesh[:,i,j] - y_transform
-                data_var=Diagonal(1.0 ./ var_mesh[:,i,j] )
-                gp_fidelity = 0.5*sum(log.(var_mesh[:,i,j] ))      
-                objective[i,j]= 0.5*diff'*data_var*diff + gp_fidelity
-
-            elseif gp_type == "sample_mean"
-                GP_sample = rand(MvNormal(mean_mesh[:,i,j],Diagonal(var_mesh[:,i,j])), sample_size)
-                GP_sample = reshape(GP_sample,(length(mean_mesh[:,i,j]),sample_size))
-                diff =(GP_sample .- y_transform)
-                inv_data_var = Diagonal(1.0 ./ (truth_var_transformed+ var_mesh[:,i,j] ))       
-                misfit = 0
-                for k = 1:sample_size
-                    misfit +=  0.5 * diff[:,k]'*inv_data_var*diff[:,k]
-                end
-                gp_fidelity = 0.5*sum(log.(var_mesh[:,i,j] ))      
-                objective[i,j] = 1/sample_size*sum(misfit) + gp_fidelity
-
-            end
+    objective = zeros((length(param1vals),length(param2vals)))
+    for i = 1:length(param1vals)
+        for j = 1:length(param2vals)
+            diff = mean_mesh[:,i,j] - y_transform
+            data_var = Diagonal(1.0 ./ var_mesh[:,i,j] )
+            gp_fidelity = 0.5*sum(log.(var_mesh[:,i,j] ))      
+            objective[i,j] = 0.5*diff'*data_var*diff + gp_fidelity
         end
     end
     minobj = minimum(objective)
     maxobj = maximum(objective)
     println("minimum of objective", minobj)
     println("maximum of objective", maxobj)
-    rhum_bd = [minimum(rhumvals), maximum(rhumvals)]
-    logtau_bd = [minimum(logtauvals), maximum(logtauvals)]
-    
-    # 
+    param1_bd = [minimum(param1vals), maximum(param1vals)]
+    param2_bd = [minimum(param2vals), maximum(param2vals)]
     
     #plot - transformed
     gr(size = (500,500))
-    n_levels = 30
-    
     objlevels = collect(minobj:(maxobj - minobj) / (n_levels - 1):maxobj)
     
     #Plot the objective function
-    plot(rhumvals,
-         logtauvals,
-         objective', #may need transpose here
-         #seriestype=:heatmap,
+    plot(param1vals,
+         param2vals,
+         objective',
          seriestype=:contour,
          levels=objlevels,
          seriescolor=cgrad(:tempo), #white = small obj., dark = big obj.
          fill=true,
          legend=false,
          grid=false,
-         xlims=rhum_bd,
-         ylims=logtau_bd,
+         xlims=param1_bd,
+         ylims=param2_bd,
          dpi=300,
          colorbar=:right,
          clims=[minobj,maxobj],
@@ -153,10 +114,10 @@ function main()
     #plot the EKI points over the top
     circ = Shape(Plots.partialcircle(0, 2π))
     
-    #get points in domain
-     rhum_in_bd = (u_flat[:,1] .> rhum_bd[1]) .& (u_flat[:,1].<rhum_bd[2])
-     logtau_in_bd = (u_flat[:,2] .> logtau_bd[1]) .& (u_flat[:,2].<logtau_bd[2])
-     u_in_bd = u_flat[(rhum_in_bd) .& (logtau_in_bd),:]
+    #get EKP points in domain
+    param1_in_bd = (u_flat[:,1] .> param1_bd[1]) .& (u_flat[:,1].<param1_bd[2])
+    param2_in_bd = (u_flat[:,2] .> param2_bd[1]) .& (u_flat[:,2].<param2_bd[2])
+    u_in_bd = u_flat[(param1_in_bd) .& (param2_in_bd),:]
     
     plot!(u_in_bd[:,1],
           u_in_bd[:,2],
@@ -165,35 +126,28 @@ function main()
           markercolor=:black,
           markersize=5,
           msw=0)
-    
-    #        vline!([true_params[1]],color=:blue,linealpha=0.5,linestyle=:dash)
-    #        hline!([true_params[2]],color=:blue,linealpha=0.5,linestyle=:dash)
-    
-    xlabel!("logit-relative humidity parameter")
-    ylabel!("log-timescale parameter (seconds)")
+        
+    xlabel!("transformed param1")
+    ylabel!("transformed param2")
     savefig(outdir*"trainpts_over_objective_transformed.pdf")
     
 
-    #untransform
-    hours = 3600.0
-    #Note the rhumvals flips the order low -> high transformed becomes high -> low!
-    #need to flip later on for contour
-    rhumvals_raw = reverse(inverse_transform_rh.(rhumvals))
-    objective_raw = reverse(objective, dims=1)
+    #untransformed 'raw' coordinates
+    param1vals_raw,param2vals_raw = transform_prior_to_real([param1vals,param2vals])
+    param1vals_raw,param2vals_raw = apply_units_transform([param1vals_raw,param2vals_raw])
+
+    u_raw_in_bd = transform_prior_to_real(u_in_bd)
+    u_raw_in_bd = apply_units_transform(u_in_bd)
     
-    logtauvals_raw = inverse_transform_t.(logtauvals) ./ hours
-    rhum_bd_raw = [minimum(rhumvals_raw), maximum(rhumvals_raw)]
-    logtau_bd_raw = [minimum(logtauvals_raw), maximum(logtauvals_raw)]
-    u_raw_in_bd = zeros(size(u_in_bd))
-    u_raw_in_bd[:,1] = inverse_transform_rh.(u_in_bd[:,1])
-    u_raw_in_bd[:,2] = inverse_transform_t.(u_in_bd[:,2]) ./ hours
+    param1_bd_raw = [minimum(param1vals_raw), maximum(param1vals_raw)]
+    param2_bd_raw = [minimum(param2vals_raw), maximum(param2vals_raw)]
       
     #plot in untransformed coordinates
     
     #Plot the objective function
     
-    plot(rhumvals_raw,
-         logtauvals_raw,
+    plot(param1vals_raw,
+         param2vals_raw,
          objective_raw', #may need transpose here
          #seriestype=:heatmap,
          seriestype=:contour,
@@ -202,8 +156,8 @@ function main()
          fill=true,
          legend=false,
          grid=false,
-         xlims=rhum_bd_raw,
-         ylims=logtau_bd_raw,
+         xlims=param1_bd_raw,
+         ylims=param2_bd_raw,
          dpi=300,
          colorbar=:right,
          clims=[minobj,maxobj],
@@ -220,9 +174,9 @@ function main()
           markersize=5,
           msw=0)
 
-    xlabel!("relative humidity parameter")
-    ylabel!("timescale parameter (hours)")
-    savefig(outdir*"trainpts_over_objective_untransformed.pdf")
+    xlabel!("param1")
+    ylabel!("param2")
+    savefig("trainpts_over_objective_untransformed.pdf")
 
 end
 
